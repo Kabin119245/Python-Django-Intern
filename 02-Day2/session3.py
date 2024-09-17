acdb15553093355482f00d3467ae2e94d8af5ac3
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-17 11:06:01 +0545

    type casting added

---
+++ 02-Day2/session3.py
@@ -0,0 +1,21 @@
+a = 5+6j
+print(a)
+print(type(a))
+
+
+x = 5+7j
+y = 7-6j
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+
+String = "ILOVEMYCOUNTRYNEPAL"
+print(String[:3])
+print(String[1:5:2])
+print(String[-1:-12:-2])
+print(String[::-1])
+print(String[14:19])
+print(String[5:])
